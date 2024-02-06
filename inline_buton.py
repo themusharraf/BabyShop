517f4509521a8db0bc40e2fa3a00e573a489efee
torabekov_08 <diyorbektorabekov08@gmail.com>
2024-02-06 18:37:01 +0500

    #torabekov

---
+++ inline_buton.py
@@ -0,0 +1,9 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import types
+
+inline = InlineKeyboardBuilder()
+inline.row(types.InlineKeyboardButton(text="Ustki kiyimlar👗"),
+inline.row(types.InlineKeyboardButton(text="Sotib olish", url="https://t.me/MYSTERIOUS_06")))
+
+
+
