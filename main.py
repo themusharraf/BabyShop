725c3effec0c7491cf572715bf362715857adb2c
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-02-10 10:45:54 +0500

    Commit

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from aiogram.filters.command import Command
 from aiogram.enums import ParseMode
 from aiogram.types import BotCommand
 from root import TOKEN
-from buttons import button, button_boy, button_girl
-from inline_button import inline_btn
-from images import *
+from buttons import button, buton
 
 dp = Dispatcher()
 
@@ -25,74 +23,28 @@ async def cmd_start(message: types.Message):
     await message.answer(f"Hello {message.from_user.full_name}", parse_mode=ParseMode.HTML, reply_markup=button)
 
     @dp.message(F.text == "O'g'il 🙍‍♂️")
-    async def cmd_boy(message: types.Message):
-        await message.answer("Bolalar kiyimlar ro'yhati", reply_markup=button_boy)
+    async def cmdd(message: types.Message):
+        await message.answer("Kiyimlar ro'yhati", reply_markup=buton)
 
         @dp.message(F.text == "Bosh kiyim 🧢")
-        async def cmd_bosh_kiyim(message: types.Message):
-            for i in bosh_kiyim_boy:
-                await message.answer_photo(photo=i, caption="Bosh kiyim 🧢", reply_markup=inline_btn)
+        async def cmdd(message: types.Message):
+            await message.answer_photo(photo="https://i.postimg.cc/jjTdmbnL/kurtka.jpg")
 
         @dp.message(F.text == "Ustki kiyim 👕")
-        async def cmd_ustki_kiyim(message: types.Message):
-            await message.answer_photo(photo="https://i.postimg.cc/jjTdmbnL/kurtka.jpg", caption="Ustki kiyim 👕",
-                                       reply_markup=inline_btn)
+        async def cmdd(message: types.Message):
+            await message.answer_photo(photo="https://i.postimg.cc/jjTdmbnL/kurtka.jpg")
 
         @dp.message(F.text == "Pastki kiyim 👖")
-        async def cmd_pastki_kiyim(message: types.Message):
-            await message.answer_photo(photo="https://i.postimg.cc/jjTdmbnL/kurtka.jpg", caption="Pastki kiyim 👖",
-                                       reply_markup=inline_btn)
+        async def cmdd(message: types.Message):
+            await message.answer_photo(photo="https://i.postimg.cc/jjTdmbnL/kurtka.jpg")
 
         @dp.message(F.text == "Oyoq kiyim 🥾")
-        async def cmd_oyoq_kiyim(message: types.Message):
-            for i in Oyoq_kiyimlar_boy:
-                await message.answer_photo(photo=i, caption="Oyoq kiyim 🥾", reply_markup=inline_btn)
+        async def cmdd(message: types.Message):
+            await message.answer_photo(photo="https://i.postimg.cc/jjTdmbnL/kurtka.jpg")
 
-        @dp.message(F.text == "🔙 Orqaga qaytish")
-        async def cmd_orqaga_boy(message: types.Message):
+        @dp.message(F.text == "Orqaga qaytish")
+        async def cmd_orqaga(message: types.Message):
             await message.answer("Orqaga qaytildi", reply_markup=button)
-            await message.delete()
-
-    @dp.message(F.text == "Qiz 🙍‍♀️")
-    async def cmd_girl(message: types.Message):
-        await message.answer("Qizlar kiyimlar ro'yhati", reply_markup=button_girl)
-
-        @dp.message(F.text == "Bosh kiyim 👒")
-        async def cmd_bosh_kiy_girl(message: types.Message):
-            for i in bosh_kiyim_girl:
-                await message.answer_photo(photo=i, caption="Bosh kiyim 👒", reply_markup=inline_btn)
-
-        @dp.message(F.text == "Ustki kiyim 👗")
-        async def cmd_ustki_kiy_girl(message: types.Message):
-            await message.answer_photo(photo="https://i.postimg.cc/SNn0h5kG/photo-94-2024-02-06-16-33-03.jpg",
-                                       caption="Ustki Kiyim 👗", reply_markup=inline_btn)
-            await message.answer_photo(photo="https://i.postimg.cc/3Rt6Mjq6/photo-89-2024-02-06-16-33-03.jpg",
-                                       caption="Ustki Kiyim 👗", reply_markup=inline_btn)
-            await message.answer_photo(photo="https://i.postimg.cc/BbdmsmBh/photo-57-2024-02-06-16-33-03.jpg",
-                                       caption="Ustki Kiyim 👗", reply_markup=inline_btn)
-            await message.answer_photo(photo="https://i.postimg.cc/fyhmyFJF/qiz.jpg",
-                                       caption="Ustki Kiyim 👗", reply_markup=inline_btn)
-            await message.answer_photo(photo="https://i.postimg.cc/TwcxKW4q/photo-96-2024-02-06-16-33-03.jpg",
-                                       caption="Ustki Kiyim 👗", reply_markup=inline_btn)
-
-        # @dp.message(F.text == "Pastki kiyim 👖")
-        # async def cmd_pastki_kiy_girl(message: types.Message):
-        #
-        # @dp.message(F.text == "Oyoq kiyim 👠")
-        # async def cmd_oyoq_kiy_girl(message: types.Message):
-
-        @dp.message(F.text == "🔙 Orqaga qaytish")
-        async def cmd_orqaga_girl(message: types.Message):
-            await message.answer("Orqaga qaytildi", reply_markup=button)
-            await message.delete()
-
-
-@dp.callback_query(F.data == "alert")
-async def send_random_value(callback: types.CallbackQuery):
-    await callback.answer(
-        text="Batafsil ma'lumot olish uchun bosing:\nSotib Olish 🛒 ni bosing va Adminga murojaat qiling ✅",
-        show_alert=True
-    )
 
 
 async def main():
